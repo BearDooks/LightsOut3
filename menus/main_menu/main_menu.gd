6cd5165e5f12dbb29d66d4176dc2b5622bebddd3
Chuck <chuck.lindblom@gmail.com>
2021-01-23 14:17:43 -0500

    Move functions around

--- menus/main_menu/main_menu.gd
+++ menus/main_menu/main_menu.gd
@@ -6,9 +6,9 @@ extends Control
 func _ready() -> void:
 	audio.play_main_menu_theme()
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene("res://menus/options_menu/options_menu.tscn")
-
 func _on_new_game_pressed() -> void:
 	global.set_game_type('easy')
 	get_tree().change_scene("res://assets/level/level.tscn")
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene("res://menus/options_menu/options_menu.tscn")
