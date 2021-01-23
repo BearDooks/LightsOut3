ada6d8357f4f4e17c0f793ccb4b1a7eb3d6530ba
Chuck <chuck.lindblom@gmail.com>
2021-01-23 14:05:22 -0500

    Update saving and loading and fix some typos

--- audio/audio.gd
+++ audio/audio.gd
@@ -1,20 +1,20 @@
 extends Node
 
 # Connections
-onready var music_player = get_node("music_player")
+onready var music_player : Node = get_node("music_player")
 
 # Variables
-var currently_playing
+var currently_playing : String = ""
 
 # Music
-var main_menu_theme = "res://audio/A million light years between us.ogg"
-var level_theme = "res://audio/enchanted tiki 86.ogg"
+var main_menu_theme : String = "res://audio/A million light years between us.ogg"
+var level_theme : String = "res://audio/enchanted tiki 86.ogg"
 
-var mute_music = false
-var mute_sfx = false
+var mute_music : bool = false
+var mute_sfx : bool = false
 
 # A function to toggle if the music for the game plays or not
-func toggle_music():
+func toggle_music() -> void:
 	if music_player.playing:
 		music_player.stop()
 	if mute_music == true:
@@ -22,7 +22,7 @@ func toggle_music():
 	else:
 		mute_music = true
 
-func play_main_menu_theme():
+func play_main_menu_theme() -> void:
 	if currently_playing != "main_menu_theme":
 		music_player.stop()
 		if mute_music != true:
@@ -30,7 +30,7 @@ func play_main_menu_theme():
 			currently_playing = "main_menu_theme"
 			music_player.play()
 
-func play_level_theme():
+func play_level_theme() -> void:
 	if currently_playing != "level_theme":
 		music_player.stop()
 		if mute_music != true:
